a031694f19f1eb20e159d6911c68deebc2372b19
dvdzz <dvdzz76@icloud.com>
2019-09-12 18:56:38 +0000

    finished quiz 2 including the bonus

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -4,4 +4,19 @@ class AnimalsController < ApplicationController
         @animals = Animal.all
     end
 
+    def new
+        @animal = Animal.new
+    end
+
+    def create
+        Animal.create(animal_params)
+        redirect_to root_path
+    end
+
+    private
+
+    def animal_params
+        params.require(:animal).permit(:name, :description)
+    end
+
 end
